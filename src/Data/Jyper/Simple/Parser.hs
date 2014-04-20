de84a6059eb5617f0eacbee28ef351319ed1fd4c
amutake <amutake.s@gmail.com>
2014-04-20 21:27:24 +0900

    Value or concrete type

--- src/Data/Jyper/Simple/Parser.hs
+++ src/Data/Jyper/Simple/Parser.hs
@@ -2,7 +2,9 @@
 
 module Data.Jyper.Simple.Parser where
 
-import Text.Peggy hiding (parse, Choice)
+import Data.Char
+import Numeric
+import Text.Peggy (parseFile, ParseError, peggy, defaultDelimiter, space)
 
 import Data.Jyper.Simple.Types
 
@@ -22,7 +24,43 @@ ident ::: String = [a-zA-Z] [a-zA-Z0-9_]* { $1 : $2 }
 body :: Body
   = "{" "}" { Object [] }
   / "{" fields "}" { Object $1 }
-  / concrete ("|" concrete)* { Choice ($1 : $2) }
+  / valueOrType ("|" valueOrType)* { Choice ($1 : $2) }
+
+valueOrType :: Either Value ConcreteType
+  = value { Left $1 }
+  / concrete { Right $1 }
+
+value :: Value
+  = "null" { NullValue }
+  / "true" { BoolValue True }
+  / "false" { BoolValue False }
+  / string { StringValue $1 }
+  / int { IntValue $1 }
+
+string ::: String
+  = '\"' char* '\"'
+
+char :: Char
+  = '\\' escChar
+  / [^\"\\]
+
+escChar :: Char
+  = '\"' { '\"' }
+  / '\\' { '\\' }
+  / '/' { '/' }
+  / 'b' { '\b' }
+  / 'f' { '\f' }
+  / 'n' { '\n' }
+  / 'r' { '\r' }
+  / 't' { '\t' }
+  / 'u' hex hex hex hex { chr $ fst $ head $ readHex [$1, $2, $3, $4] }
+
+hex :: Char = [0-9a-zA-Z]
+
+int :: Int
+  = "0" { 0 }
+  / [1-9] [0-9]* { read ($1 : $2) }
+  / "-" [1-9] [0-9]* { negate (read ($1 : $2)) }
 
 concrete :: ConcreteType
   = ident "[" concrete ("," concrete)* "]" { ConcreteType $1 ($2 : $3) }
--- src/Data/Jyper/Simple/Types.hs
+++ src/Data/Jyper/Simple/Types.hs
@@ -13,11 +13,20 @@ instance Show TypeName where
   show (TypeName constr []) = constr
   show (TypeName constr params) = constr ++ "[" ++ intercalate ", " params ++ "]"
 
-data Body = Object [Field] | Choice [ConcreteType]
+data Body = Object [Field] | Choice [Either Value ConcreteType]
 
 instance Show Body where
   show (Object fields) = "{\n" ++ intercalate ",\n" (map (("  " ++) . show) fields) ++ "\n}"
-  show (Choice types) = intercalate " | " (map show types)
+  show (Choice types) = intercalate " | " (map (either show show) types)
+
+data Value = NullValue | BoolValue Bool | StringValue String | IntValue Int
+
+instance Show Value where
+  show NullValue = "null"
+  show (BoolValue True) = "true"
+  show (BoolValue False) = "false"
+  show (StringValue str) = "\"" ++ str ++ "\"" -- show str
+  show (IntValue n) = show n
 
 data ConcreteType = ConcreteType String [ConcreteType]
 
