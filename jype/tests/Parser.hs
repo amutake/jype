0b9599c48e2e7588eb7db34be9eafedf35b23c5a
amutake <amutake.s@gmail.com>
2014-04-24 18:16:26 +0900

    Add a test case

--- jype/tests/Parser.hs
+++ jype/tests/Parser.hs
@@ -11,46 +11,64 @@ import Data.Jype.Syntax
 
 parserTests :: IO ()
 parserTests = defaultMain $ testGroup "parser"
-    [ testCase "no description" $ common $
-        [ Decl (TypeName "user" []) (Object
-            [ Field "id" (ConcreteType "int" []) [] Nothing
-            , Field "name" (ConcreteType "string" []) [] Nothing
-            ]) []
-        , Decl (TypeName "either" ["a", "b"]) (Choice
-            [ Right (ConcreteType "a" [])
-            , Right (ConcreteType "b" [])
-            ]) []
+    [ testGroup "parse . show == id" $
+        [ testCase "no description" $ common $
+            [ Decl (TypeName "user" []) (Object
+                [ Field "id" (ConcreteType "int" []) [] Nothing
+                , Field "name" (ConcreteType "string" []) [] Nothing
+                ]) []
+            , Decl (TypeName "either" ["a", "b"]) (Choice
+                [ Right (ConcreteType "a" [])
+                , Right (ConcreteType "b" [])
+                ]) []
+            ]
+        , testCase "declaration description" $ common $
+            [ Decl (TypeName "id" ["a"]) (Choice
+                [ Right (ConcreteType "a" [])
+                ]) [ "this is descriptions", "hogehoge", "yaayaa" ]
+            ]
+        , testCase "field description (before)" $ common $
+            [ Decl (TypeName "id" ["a"]) (Object
+                [ Field "a" (ConcreteType "a" []) [ "hoge", "fuga", "moge" ] Nothing
+                , Field "a" (ConcreteType "a" []) [ "hoge", "fuga", "moge" ] Nothing
+                , Field "a" (ConcreteType "a" []) [ "hoge", "fuga", "moge" ] Nothing
+                ]) []
+            ]
+        , testCase "field description (after)" $ common $
+            [ Decl (TypeName "id" ["a"]) (Object
+                [ Field "a" (ConcreteType "a" []) [] (Just "hogehogehoge")
+                , Field "a" (ConcreteType "a" []) [] (Just "hogehogehoge")
+                , Field "a" (ConcreteType "a" []) [] (Just "hogehogehoge")
+                ]) []
+            ]
+        , testCase "complex description" $ common $
+            [ Decl (TypeName "hoge" ["a"]) (Object
+                [ Field "hoge" (ConcreteType "a" []) ["this", "is", "hoge"] (Just "hoge")
+                , Field "fuga" (ConcreteType "int" []) [] Nothing
+                , Field "moge" (ConcreteType "string" []) ["this", "is", "moge"] (Just "moge")
+                ]) ["this", "is", "hoge"]
+            , Decl (TypeName "hoge" ["a"]) (Object
+                [ Field "hoge" (ConcreteType "a" []) ["this", "is", "hoge"] Nothing
+                , Field "fuga" (ConcreteType "int" []) ["this", "is", "fuga"] (Just "fuga")
+                , Field "moge" (ConcreteType "string" []) [] (Just "moge")
+                ]) ["this", "is", "hoge"]
+            ]
         ]
-    , testCase "declaration description" $ common $
-        [ Decl (TypeName "id" ["a"]) (Choice
-            [ Right (ConcreteType "a" [])
-            ]) [ "this is descriptions", "hogehoge", "yaayaa" ]
-        ]
-    , testCase "field description (before)" $ common $
-        [ Decl (TypeName "id" ["a"]) (Object
-            [ Field "a" (ConcreteType "a" []) [ "hoge", "fuga", "moge" ] Nothing
-            , Field "a" (ConcreteType "a" []) [ "hoge", "fuga", "moge" ] Nothing
-            , Field "a" (ConcreteType "a" []) [ "hoge", "fuga", "moge" ] Nothing
-            ]) []
-        ]
-    , testCase "field description (after)" $ common $
-        [ Decl (TypeName "id" ["a"]) (Object
-            [ Field "a" (ConcreteType "a" []) [] (Just "hogehogehoge")
-            , Field "a" (ConcreteType "a" []) [] (Just "hogehogehoge")
-            , Field "a" (ConcreteType "a" []) [] (Just "hogehogehoge")
-            ]) []
-        ]
-    , testCase "complex description" $ common $
-        [ Decl (TypeName "hoge" ["a"]) (Object
-            [ Field "hoge" (ConcreteType "a" []) ["this", "is", "hoge"] (Just "hoge")
-            , Field "fuga" (ConcreteType "int" []) [] Nothing
-            , Field "moge" (ConcreteType "string" []) ["this", "is", "moge"] (Just "moge")
-            ]) ["this", "is", "hoge"]
-        , Decl (TypeName "hoge" ["a"]) (Object
-            [ Field "hoge" (ConcreteType "a" []) ["this", "is", "hoge"] Nothing
-            , Field "fuga" (ConcreteType "int" []) ["this", "is", "fuga"] (Just "fuga")
-            , Field "moge" (ConcreteType "string" []) [] (Just "moge")
-            ]) ["this", "is", "hoge"]
+    , testGroup "parse == decls" $
+        [ testCase "complex" $ parseEqual "./tests/jypefiles/1.jype" $
+            [ Decl (TypeName "hoge" ["a","b","c"]) (Object
+                [ Field "key" (ConcreteType "type" []) [] (Just "hoge")
+                , Field "key" (ConcreteType "a" []) ["hoge"] Nothing
+                , Field "key" (ConcreteType "either" [ConcreteType "b" [], ConcreteType "c" []]) ["hoge"] Nothing
+                ]) ["hoge   # hoge", "hoge", "hoge"]
+            , Decl (TypeName "hoge" []) (Choice
+                [ Left (IntValue (-1))
+                , Left (StringValue "hoge")
+                , Left (IntValue 1)
+                , Left (BoolValue True)
+                , Left (BoolValue False)
+                ]) ["hoge", "hoge"]
+            ]
         ]
     ]
 
@@ -63,3 +81,10 @@ newtype Decls = Decls [Decl] deriving Eq
 
 instance Show Decls where
     show (Decls decls) = ("\n" ++) . unlines . map show $ decls
+
+parseEqual :: FilePath -> [Decl] -> Expectation
+parseEqual path decls = do
+    result <- parseFile path
+    case result of
+        Left err -> assertFailure $ show err ++ show (Decls decls)
+        Right decls' -> Decls decls' `shouldBe` Decls decls
