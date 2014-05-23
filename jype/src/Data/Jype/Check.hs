fce01536363142087774aa7c54c5494e1938479d
amutake <amutake.s@gmail.com>
2014-05-23 12:52:55 +0900

    Check whether containing unknown type

--- jype/src/Data/Jype/Check.hs
+++ jype/src/Data/Jype/Check.hs
@@ -12,7 +12,7 @@ check ds
     | null errors = Right ds
     | otherwise = Left $ JypeCheckError errors
   where
-    errors = checkTypeNames ds ++ checkKeys ds ++ checkConcreteType ds
+    errors = checkTypeNames ds ++ checkKeys ds ++ checkUnknownType ds
 
 dup :: Eq a => [a] -> [a]
 dup [] = []
@@ -20,9 +20,11 @@ dup (x:xs)
     | elem x xs = x : dup (xs \\ [x])
     | otherwise = dup xs
 
+-- | Check whether the name that decleared type is duplicated
 checkTypeNames :: [Decl] -> [String]
 checkTypeNames = map ("duplicate type: " ++) . dup . map (typeNameConstr . declTypeName)
 
+-- | Check whether keys in fields are duplicated
 checkKeys :: [Decl] -> [String]
 checkKeys = (>>= checkKeys')
   where
@@ -30,5 +32,18 @@ checkKeys = (>>= checkKeys')
         map (("duplicate key in type " ++ show name ++ ": ") ++) . dup . map fieldKey $ fs
     checkKeys' _  = []
 
-checkConcreteType :: [Decl] -> [String]
-checkConcreteType = const []
+-- | Check whether concrete types are defined
+checkUnknownType :: [Decl] -> [String]
+checkUnknownType decls = decls >>= checkDecl
+  where
+    names = map (typeNameConstr . declTypeName) decls
+    checkDecl (Decl typ@(TypeName _ params) (Object fields) _) =
+        fields >>= (checkConc (show typ) params . fieldType)
+    checkDecl (Decl typ@(TypeName _ params) (Choice choices) _) =
+        choices >>= (checkChoice (show typ) params . choiceEither)
+    checkDecl (Decl _ Primitive _) = []
+    checkConc typ params (ConcreteType name paramConcs)
+        | elem name (names ++ params) = paramConcs >>= checkConc typ params
+        | otherwise = ("unknown type in type " ++ typ ++ ": " ++ name) : (paramConcs >>= checkConc typ params)
+    checkChoice _ _ (Left _) = []
+    checkChoice typ params (Right conc) = checkConc typ params conc
