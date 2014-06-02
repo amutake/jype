e3e2db117a7f15bdac8cfdc4f6ba8be4902ca961
amutake <amutake.s@gmail.com>
2014-06-02 22:17:58 +0900

    Modify primitives

--- jype/src/Data/Jype/Primitives.hs
+++ jype/src/Data/Jype/Primitives.hs
@@ -9,7 +9,8 @@ import Data.Jype.Syntax
 primitives :: [Decl]
 primitives =
     [ Decl (TypeName "int" []) Primitive ["Integer type"]
+    , Decl (TypeName "float" []) Primitive ["Float type"]
     , Decl (TypeName "string" []) Primitive ["String type"]
     , Decl (TypeName "array" ["a"]) Primitive ["Array type of type 'a'"]
-    , Decl (TypeName "option" ["a"]) Primitive ["option[a] represents either a type 'a' or there is no key."]
+    , Decl (TypeName "option" ["a"]) Primitive ["option[a] represents either just type 'a' or there is no value."]
     ]
